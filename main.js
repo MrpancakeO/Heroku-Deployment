e69fd079c5a1d4a41608299783f2c489fa5cb271
Brandon Chansamone <braakson@yahoo.com>
2021-05-05 20:52:31 -0600

    added codes

---
+++ main.js
@@ -0,0 +1,93 @@
+"use strict";
+
+// const { db } = require("./models/user");
+
+ //const { connect } = require("mongodb");
+
+const express = require("express"), 
+app = express(),
+router = require("./routes/index"),
+homeController = require("./controllers/homeController"),
+errorController = require("./controllers/errorController"),
+layouts = require("express-ejs-layouts"), 
+mongoose = require("mongoose"),
+methodOverride = require("method-override"),
+subscribersController = require("./controllers/subscribersController"),
+usersController = require("./controllers/usersController"),
+coursesController = require("./controllers/coursesController"),
+passport = require("passport"),
+cookieParser = require("cookie-parser"),
+expressSession = require("express-session"),
+expressValidator = require("express-validator"),
+connectFlash = require("connect-flash"),
+User = require("./models/user");
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/recipe_db", 
+{useNewUrlParser: true});
+mongoose.set("useCreateIndex", true);
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+    console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
+
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(layouts);
+app.use(expressValidator());
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+
+app.use(methodOverride("_method", {methods: ['POST', 'GET']}));
+
+app.use(express.json());
+
+app.use(cookieParser("my_passcode"));
+app.use(expressSession({
+    secret: "my_passcode",
+    cookie: {
+        maxAge: 360000
+    },
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(connectFlash());
+
+
+
+
+
+
+app.use((req, res, next) => {
+    res.locals.flashMessages = req.flash();
+    res.locals.loggedIn = req.isAuthenticated();
+    res.locals.currentUser = req.user;
+    next();
+})
+
+
+
+
+
+
+app.use("/", router);
+
+const server = app.listen(app.get("port"), () => {
+    console.log(`Server is running on port: ${app.get("port")}`);
+});
